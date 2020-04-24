2b9a96c84afb5e20375addb87c51f1baccfc4930
codessbabe <oana.anghelachi@gmail.com>
2020-04-24 14:32:35 +0200

    Updates

---
+++ precache-manifest.baf618d5d98f72296b57aab6bce6ad50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916c5880674b9dd79636695b32b2a0b0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "909e07606748dfd517ae",
+    "url": "/monsters-rolodex/static/css/main.c42c8b82.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "909e07606748dfd517ae",
+    "url": "/monsters-rolodex/static/js/main.02d4a529.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
